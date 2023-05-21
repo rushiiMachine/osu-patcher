8096e916edbaf226f1acf1faed9154628db1a047
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2023-05-21 10:30:30 -0700

    wip: initial project

---
+++ osu-patcher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using HoLLy.ManagedInjector;
+
+namespace osu_patcher;
+
+internal class Program
+{
+    public static void Main(string[] _)
+    {
+        Process.Start("C:\\osu!\\osu!.exe", "-devserver example.com");
+
+        var osuProc = Process.GetProcessesByName("osu!").FirstOrDefault()
+                      ?? throw new Exception("osu! is not running!");
+
+        Thread.Sleep(500);
+
+        var injectable = new InjectableProcess((uint)osuProc.Id);
+
+        injectable.Inject(
+            "C:\\Projects\\_personal\\osu-patcher\\osu-patcher-hook\\bin\\Debug\\osu_patcher_hook.dll",
+            "osu_patcher_hook.EntryPoint",
+            "Initialize");
+    }
+}
