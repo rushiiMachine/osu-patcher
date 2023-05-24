7d01f5fd524ce69b3c5527959de520ed29f1a12d
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2023-05-23 18:24:12 -0700

    feat: check connected server before injecting

--- OsuInjector/Program.cs
+++ OsuInjector/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Threading;
+using System.Management;
 using HoLLy.ManagedInjector;
 
 namespace OsuInjector
@@ -13,26 +11,54 @@ namespace OsuInjector
         {
             try
             {
-                var hookDllPath = Path.Combine(typeof(Program).Assembly.Location, "..\\osu!.hook.dll");
+                var hookDllPath = Path.GetFullPath(typeof(Program).Assembly.Location + "\\..\\osu!.hook.dll");
+                Console.WriteLine(hookDllPath);
 
                 if (!File.Exists(hookDllPath))
                     throw new Exception("Unable to find osu!.hook.dll in the current directory!");
 
-                Process.Start("C:\\osu!\\osu!.exe", "-devserver example.com");
-
-                var osuProc = Process.GetProcessesByName("osu!").FirstOrDefault()
-                              ?? throw new Exception("osu! is not running!");
-
-                Thread.Sleep(1000);
-
-                var injectable = new InjectableProcess((uint)osuProc.Id);
-                injectable.Inject(hookDllPath, "OsuHook.MainHook", "Initialize");
+                using (var proc = new InjectableProcess(GetOsuPid()))
+                {
+                    proc.Inject(hookDllPath, "OsuHook.MainHook", "Initialize");
+                }
             }
             catch (Exception e)
             {
                 Console.Error.WriteLine(e);
                 Console.Read();
             }
+        }
+
+        /// <summary>
+        ///     Find a <c>osu!.exe</c> process that has either <c>-devserver akatsuki.</c> or
+        ///     <c>-devserver example.com</c> in the cli arguments.
+        /// </summary>
+        /// <returns>The process id of the first matching process.</returns>
+        /// <exception cref="Exception">If found invalid osu! process or no process at all.</exception>
+        private static uint GetOsuPid()
+        {
+            using (var mgmt = new ManagementClass("Win32_Process"))
+            using (var processes = mgmt.GetInstances())
+            {
+                foreach (var process in processes)
+                {
+                    var exeName = (string)process["Name"];
+
+                    if (exeName == "osu!.exe")
+                    {
+                        var commandLine = (string)process["CommandLine"];
+                        var pid = (uint)process["ProcessId"];
+
+                        if (!commandLine.Contains(" -devserver akatsuki.") &&
+                            !commandLine.Contains(" -devserver example.com"))
+                            throw new Exception("Will not inject into osu! because it is not connected to Akatsuki!");
+
+                        return pid;
+                    }
+                }
+            }
+
+            throw new Exception("Cannot find a running osu! process!");
         }
     }
 }
