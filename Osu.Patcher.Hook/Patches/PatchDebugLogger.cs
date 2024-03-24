eb4aace531aa08b3455984d476a7f7995ef73d5c
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2024-03-24 14:41:04 -0700

    feat: PatchDebugLogger

---
+++ Osu.Patcher.Hook/Patches/PatchDebugLogger.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using JetBrains.Annotations;
+using Osu.Stubs;
+
+namespace Osu.Patcher.Hook.Patches;
+
+#if DEBUG
+
+/// <summary>
+///     Hook <c>osu_common.Helpers.Logger:Log</c> to print the live log to our console.
+/// </summary>
+[HarmonyPatch]
+[UsedImplicitly]
+public static class PatchDebugLogger
+{
+    [UsedImplicitly]
+    [HarmonyTargetMethod]
+    private static MethodBase Target() => Logger.Log.Reference;
+
+    [HarmonyPrefix]
+    [UsedImplicitly]
+    private static void Before(
+        [HarmonyArgument(0)] string message,
+        [HarmonyArgument(1)] LoggingTarget target,
+        [HarmonyArgument(2)] LogLevel level
+    ) => Console.WriteLine("{0} - {1}: {2}", level.ToString().First(), target, message);
+
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    private enum LogLevel
+    {
+        Debug,
+        Verbose,
+        Important,
+        Error,
+    }
+
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    private enum LoggingTarget
+    {
+        Runtime,
+        Network,
+        Tournament,
+        Update,
+        Performance,
+        Debug,
+    }
+}
+
+#endif
---
+++ Osu.Stubs/Logger.cs
@@ -0,0 +1,36 @@
+using JetBrains.Annotations;
+using Osu.Stubs.Opcode;
+using static System.Reflection.Emit.OpCodes;
+
+namespace Osu.Stubs;
+
+/// <summary>
+///     Original: <c>osu_common.Helpers.Logger</c>
+///     b20240102.2: <c>#=zN_Wxi7NCKC8KJdpmHc2RwYM=</c>
+/// </summary>
+[UsedImplicitly]
+public class Logger
+{
+    /// <summary>
+    ///     Original: <c>Log(string message, LoggingTarget target, LogLevel level)</c>
+    ///     b20240102.2: <c>#=zzl_m9cI=</c>
+    /// </summary>
+    [UsedImplicitly]
+    public static readonly LazyMethod Log = new(
+        "Logger#Log(...)",
+        new[]
+        {
+            Ldarg_1,
+            Ldc_I4_1,
+            Call,
+            Ldarg_0,
+            Ldarg_2,
+            Callvirt,
+            Leave_S,
+            Pop,
+            Leave_S,
+            Ret,
+        },
+        true
+    );
+}
